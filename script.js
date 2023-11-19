0c3ac262ad2116dec8643fbc37fabf07d5e110ed
DaymensCode <Daymenscode@gmail.com>
2023-11-19 16:39:47 -0500

    cleaned up code: removed some comments and moved around some functions in js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ findTimeBlockIds();
 
 
 function findTimeBlockIds() {
-timeBlockArray =  $('.time-block-container').children();
+ var timeBlockArray =  $('.time-block-container').children();
 
 for (i = 3; i < timeBlockArray.length; i++) {
   // started interval from 3 to account for the 3 undefined value children returns
@@ -29,8 +29,8 @@ for (i = 3; i < timeBlockArray.length; i++) {
   var timeBlockIdArray = timeBlockIds.split("-");
   var timeBlockIdHour = timeBlockIdArray[1];
 
-  console.log(currentHour); 
-  console.log(timeBlockIdHour);
+  console.log(currentHour); //for viewing current hour
+  console.log(timeBlockIdHour);//for viewing the hour value within html timeblock ID's
 
 
   //will set past/present/future classes and styling to each timebox relative
@@ -49,18 +49,8 @@ for (i = 3; i < timeBlockArray.length; i++) {
 
 };
 
-//for clicking on timeblocks ~~~~needs to be moved eventually
-  //if loop will test time-block click target to see if it
-  //matches the current military hour. 
-var clickTarget = $(this).parent();
-var clickTargetId = clickTarget.attr("id");
-if (clickTargetId.includes(currentHour)) {
-  console.log("Selected Timecard is the current hour");
-  console.log(clickTargetId);
-} else {
-  console.log("Selected Timecard is NOT the current hour");
-  console.log(clickTargetId);
-}
+
+
 };
 
 
@@ -69,6 +59,46 @@ var timeBlockContainer = $('.time-block-container')
 
 timeBlockContainer.on('click', '.saveBtn', function(event){
   event.preventDefault();
+  var clickTarget = $(this).parent();
+  var clickTargetId = clickTarget.attr("id");
+
+//for clicking on timeblocks ~~~~needs to be moved eventually
+  //if loop will test time-block click target to see if it
+  //matches the current military hour. 
+
+  
+
+  if (clickTargetId.includes(currentHour)) {
+    console.log("Selected Timecard is the current hour");
+    console.log(clickTargetId);
+  } else {
+    console.log("Selected Timecard is NOT the current hour");
+    console.log(clickTargetId);
+  }
+
+
+    //1)need code that will save written value into the timeblocks
+
+    //a) need to take in input value from user and set to variables
+
+    //b) need to have that saved input variables saved onto local storage
+
+    //c) need to create object that will take in these local storage values
+    // ===========key for the object should be the timeblock id from local
+    // ===========value for object will be user input from local
+    //d) need to 2 push local storage data into object
+
+    //e) need to set this object as a value to local storage(MUST PERSIST IN LOCAL)
+
+    //d) need code that will take in these object local storage values and set to variables
+
+    //f) need code that will write these object value variables onto the timeblocks in html
+  function saveEvent(event) {
+  
+  };
+
+});
+
 
 
   // var timeBlockID; // will be used to take in the selected time-block container
@@ -78,44 +108,14 @@ timeBlockContainer.on('click', '.saveBtn', function(event){
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  function saveEvent(event) {
-  
-  };
-
-});
-console.log("Current Hour: " + currentHour)
-
 
 // test push flow comment
 
-
-// 1) functionality to display date in real time, soon as page opens +++++++++++++++++++++++++++++++++++++++++
-// q) need a function that will  set up a live date
-// b) need to set it up in func init() so it displays soon as page opens
-// c) if time permits, maybe allow functionality for time-zone selection for this
-
-// 2) need to set up hourly event blocks, enough to span length from 9~5pm (9 blocks)
-// a) need to ensure this page height will allow for scrolling
-// b) give proper sizing to each event block so it looks neat and easily readable +++++++++++++++++++++++++++++
-
-// 3) need to make sure each block is color coded depending on corelation to the hour of the day  (do the color portion last)
-// piece of code that might help with color adoptation   $('element').css('style', 'value/attribute')
-// a) need color for past, present, and future
-// b) needs functionality to register user's timezone
-// c) needs functionality that will interact with user's timezone
-// d) need boolean variables that will toggle based off of user's timezone (past present future)
-// e) need functionality that will highlight current hour block
-// f) need functionality that will use these toggle variables to set block colors
-
 // 4) clicking on any hour block allows for entering and saving of new events +++++++++++++++++++++++++++++++++
 //!!!Might be able to use form functionality from Mod5 to facilitate some of this
-//!!!Might be able to use .eq(i) to single out some child elements in DOM
-// a) need functionality that will set these blocks as buttons
-// b) need to style/html a prompt or form to fill out for new events
-// c) need buttons to take in user inputs for new events
-// d) need to set these inputs to variables
-// e) need to create object array for packaging of these input variables
-// f) need functionality that will allow for pushing of these variables into the object
+
+
+
 
 
 // 5) need save button in this form for entering events
